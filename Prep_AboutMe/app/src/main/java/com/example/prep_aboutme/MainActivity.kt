8e14b2dc64e241f997d7b0377c30f82b8973711e
Thomas Scheibelhofer <thomas.scheibelhofer@live.at>
2021-04-23 15:11:36 +0200

    Codelabs updates

--- Prep_AboutMe/app/src/main/java/com/example/prep_aboutme/MainActivity.kt
+++ Prep_AboutMe/app/src/main/java/com/example/prep_aboutme/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.prep_aboutme
 
+import android.content.Context
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.prep_aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Thomas Scheibelhofer")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            setNickname()
+        }
+
+        binding.nicknameText.setOnClickListener { updateNickname() }
+
     }
+
+    private fun setNickname() {
+
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+            doneButton.visibility = View.GONE
+        }
+
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.doneButton.windowToken, 0)
+
+    }
+
+    private fun updateNickname() {
+
+        binding.apply {
+            nicknameText.visibility = View.GONE
+            nicknameEdit.visibility = View.VISIBLE
+            doneButton.visibility = View.VISIBLE
+            nicknameEdit.requestFocus()
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.nicknameEdit, 0)
+
+    }
+
+
 }
